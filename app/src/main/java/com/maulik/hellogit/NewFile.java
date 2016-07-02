747bdb5fa7bf5b99d02f5cdcc1d075a2efed8396
Maulik <mauleek143@gmail.com>
2016-07-03 00:16:48 +0530

    new file created on date 12:16  03-07-16

---
+++ app/src/main/java/com/maulik/hellogit/NewFile.java
@@ -0,0 +1,17 @@
+package com.maulik.hellogit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Maulik on 7/3/2016.
+ */
+
+public class NewFile  extends Activity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
